7e967ba558209c501412a3cfe9c06b37c5f08a62
rathodvinod5 <rathodvinod5@gmail.com>
2026-06-05 21:39:14 +0530

    added files for module level for instructions

--- programs/sol-stable/src/lib.rs
+++ programs/sol-stable/src/lib.rs
@@ -1,5 +1,11 @@
 use anchor_lang::prelude::*;
 
+pub mod instructions;
+use instructions::*;
+
+pub mod state;
+use state::*;
+
 declare_id!("6sntG2TygfQFj1s9Sh4MpqPf4SWFGiuipE867tzt4wez");
 
 #[program]
