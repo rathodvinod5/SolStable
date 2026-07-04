85a90f5dc99fc33dc27d6e874cbd06931fe795ab
rathodvinod5 <rathodvinod5@gmail.com>
2026-07-04 11:40:17 +0530

    refactored files and folders according the their role

---
+++ programs/sol-stable/src/instructions/stablecoin/mod.rs
@@ -0,0 +1,5 @@
+pub mod burn_stablecoin;
+pub use burn_stablecoin::*;
+
+pub mod mint_stablecoin;
+pub use mint_stablecoin::*;
---
+++ programs/sol-stable/src/instructions/vault/mod.rs
@@ -0,0 +1,8 @@
+pub mod create_vault;
+pub use create_vault::*;
+
+pub mod deposit_collateral;
+pub use deposit_collateral::*;
+
+pub mod withdraw_collateral;
+pub use withdraw_collateral::*;
